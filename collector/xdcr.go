fb99192bb5dd481d24332f08ed4f99e7f12c211f
Adel Abdelhak <adel.abdelhak.lille@gmail.com>
2019-01-02 23:57:19 +0100

    Get xdcr metrics from self node only (#16)

--- collector/xdcr.go
+++ collector/xdcr.go
@@ -13,22 +13,16 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Task (/pools/default/tasks)
-type Task struct {
-	Type   string `json:"type"`
-	Status string `json:"status"`
-	ID     string `json:"id"`
-}
-
-// XDCRExporter describes the exporter object.
+// XDCRExporter describes the exporter object
 type XDCRExporter struct {
-	context Context
-	route   string
+	context    Context
+	route      string
 	errorCount *p.GaugeVec
+	metrics    map[string]*p.Desc
 }
 
-// NewXDCRExporter instantiates the Exporter with the URI and metrics.
-func NewXDCRExporter(context Context) (*XDCRExporter, error) {
+// NewXDCRExporter instantiates the Exporter with the URI and metrics
+func NewXDCRExporter(c Context) (*XDCRExporter, error) {
 	xdcrMetrics, err := GetMetricsFromFile("xdcr")
 	if err != nil {
 		return &XDCRExporter{}, err
@@ -39,7 +33,7 @@ func NewXDCRExporter(context Context) (*XDCRExporter, error) {
 		metrics[metric.ID] = p.NewDesc(fqName, metric.Description, metric.Labels, nil)
 	}
 	return &XDCRExporter{
-		context: context,
+		context: c,
 		route:   xdcrMetrics.Route,
 		errorCount: p.NewGaugeVec(p.GaugeOpts{
 			Name: p.BuildFQName("cb", xdcrMetrics.Name, "error_count"),
@@ -49,7 +43,7 @@ func NewXDCRExporter(context Context) (*XDCRExporter, error) {
 	}, nil
 }
 
-// Describe describes exported metrics.
+// Describe describes exported metrics
 func (e *XDCRExporter) Describe(ch chan<- *p.Desc) {
 	e.errorCount.Describe(ch)
 	for _, metric := range e.metrics {
@@ -57,32 +51,47 @@ func (e *XDCRExporter) Describe(ch chan<- *p.Desc) {
 	}
 }
 
-// Collect fetches data for each exported metric.
+// Collect fetches data for each exported metric
 func (e *XDCRExporter) Collect(ch chan<- p.Metric) {
-	// get task list where xdcr are listed
+	// get task list to retrieve active XDCR links
 	body, err := Fetch(e.context, "/pools/default/tasks")
 	if err != nil {
-		log.Error("Error when retrieving XDCR data. XDCR metrics won't be scraped")
+		log.Error("Could not retrieve tasks data: XDCR metrics won't be scraped")
 		return
 	}
-	var tasks []Task
+	var tasks []struct {
+		Type   string   `json:"type"`
+		Status string   `json:"status"`
+		ID     string   `json:"id"`
+		Errors []string `json:"errors"`
+	}
 	err = json.Unmarshal(body, &tasks)
 	if err != nil {
-		log.Error("Could not unmarshal tasks data")
+		log.Error("Could not unmarshal tasks data: XDCR metrics won't be scraped")
 		return
 	}
 
-	// create urls from task list for each xdcr metric
 	var routes []string
+	remoteClusters := make(map[string]string, 0)
 	errorsCount := make(map[string]int, 0)
 	for _, task := range tasks {
 		if task.Type == "xdcr" {
-			longID := strings.Split(task.ID, "/") // id is in the form uuid/src/dest
-			for id := range e.metrics {
-				route := fmt.Sprintf("%s/%s/stats/replications%%2F%s%%2F%s%%2F%s%%2F%s", e.route, longID[1], longID[0], longID[1], longID[2], id)
+			// create url for each xdcr metric
+			taskID := strings.Split(task.ID, "/")
+			if len(taskID) < 3 {
+				log.Error("Task ID doesn't have the expected format (uuid/src/dest): ", taskID)
+				continue
+			}
+			uuid, src, dest := taskID[0], taskID[1], taskID[2]
+			for metricID := range e.metrics {
+				route := fmt.Sprintf("%s/%s/stats/replications%%2F%s%%2F%s%%2F%s%%2F%s", e.route, src, uuid, src, dest, metricID)
 				routes = append(routes, route)
 			}
+
+			// get error count based on number of error messages in tasks endpoint
 			errorsCount[uuid] = len(task.Errors)
+
+			// associate remote clusters names with uuid for labelling
 			body, err = Fetch(e.context, "/pools/default/remoteClusters")
 			if err != nil {
 				log.Error("Could not retrieve remote clusters data")
@@ -98,42 +107,59 @@ func (e *XDCRExporter) Collect(ch chan<- p.Metric) {
 			for _, rc := range tmpRemoteClusters {
 				remoteClusters[rc.UUID] = rc.Name
 			}
+		}
+	}
+
+	// get hostname of the node
+	body, err = Fetch(e.context, "/nodes/self")
+	if err != nil {
+		log.Error("Could not retrieve node data: XDCR metrics won't be scraped")
+		return
+	}
+	var node struct {
+		Hostname string `json:"hostname"`
+	}
+	err = json.Unmarshal(body, &node)
+	if err != nil {
+		log.Error("Could not unmarshal node data: XDCR metrics won't be scraped")
+		return
+	}
 
 	// fetch all bodies from urls created above
 	bodies := MultiFetch(e.context, routes)
 
 	var currentUUID string
 	for route, body := range bodies {
+		// split back url to get uuid src & dest buckets and metric name
 		longID := strings.Split(route, "%2F")
-		uuid, src, dest, id := longID[1], longID[2], longID[3], longID[4]
+		uuid, src, dest, metricID := longID[1], longID[2], longID[3], longID[4]
 
-		var xdcr map[string]interface{}
+		// get node stats object
+		var xdcr struct {
+			NodeStats map[string]interface{} `json:"nodeStats"`
+		}
 		err := json.Unmarshal(body, &xdcr)
 		if err != nil {
-			log.Error("Could not unmarshal XDCR data for remote " + uuid + " and metric " + id)
+			log.Error("Could not unmarshal XDCR data for remote " + uuid + " and metric " + metricID)
 			continue
 		}
-		for node, values := range xdcr["nodeStats"].(map[string]interface{}) {
-			list := values.([]interface{})
-			if len(list) == 0 {
-				log.Warn("No value found for " + id + " metric in remote " + uuid)
-				continue
-			}
-			var metric *p.Desc
-			for mid, m := range e.metrics {
-				if mid == id {
-					metric = m
-					break
-				}
-			}
+		if _, ok := xdcr.NodeStats[node.Hostname].([]interface{}); !ok {
+			continue
+		}
 
-			var value float64
-			switch v := list[len(list)-1].(type) {
-			case float64:
-				value = v
-			case int:
-				value = float64(v)
-			}
+		list := xdcr.NodeStats[node.Hostname].([]interface{})
+		if len(list) == 0 {
+			log.Debug("No value found for " + metricID + " metric in remote " + uuid)
+			continue
+		}
+
+		var value float64
+		switch v := list[len(list)-1].(type) {
+		case float64:
+			value = v
+		case int:
+			value = float64(v)
+		}
 
 		if currentUUID != uuid {
 			currentUUID = uuid
